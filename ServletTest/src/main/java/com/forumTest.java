7119a5c6db644a323fefbf5ffc826d75b9219142
Lina Ait Ider <103391128+LinaAitIder@users.noreply.github.com>
2025-03-21 00:28:20 +0000

    Add files via upload

---
+++ ServletTest/src/main/java/com/forumTest.java
@@ -0,0 +1,36 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class forumTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	
+    public forumTest() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		doGet(request, response);
+		String name = request.getParameter("name");
+		PrintWriter out = response.getWriter();
+		out.println(" Bonjour "+name);
+	}
+
+}
